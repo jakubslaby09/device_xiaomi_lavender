c98eddc5ab91d15589e8ffc4b86c5b9f6c8c4444
Omar Mohamed <omarcoptan9@gmail.com>
2023-04-26 15:10:56 +0200

    lavender: Configure vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,9 @@
 # Dt
 git clone --depth=1 https://github.com/OmarAlCoptan/vendor_xiaomi_lavender.git -b tiramisu vendor/xiaomi/lavender
 git clone --depth=1 https://github.com/OmarAlCoptan/kernel_xiaomi_lavender.git -b tiramisu kernel/xiaomi/lavender
+
+# Hardware Lineage Compat
+git clone --depth=1 https://github.com/LineageOS/android_hardware_lineage_compat hardware/lineage/compat
+
+# GcamGOPrebuilt
+git clone --depth=1 https://github.com/Evolution-X/packages_apps_GCamGOPrebuilt.git packages/apps/GCamGOPrebuilt
